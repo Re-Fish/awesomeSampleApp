1522f4ed4b861879503b2e5b8769d7688ed8fae8
Roman Klyucharev <refish@tuta.io>
2020-10-13 21:59:06 +0500

    Code optimised

--- app/src/main/java/com/example/myawesomeapplication/MainActivity.kt
+++ app/src/main/java/com/example/myawesomeapplication/MainActivity.kt
@@ -13,19 +13,19 @@ class MainActivity : AppCompatActivity() {
 
         button1.setOnClickListener {
             Log.e("@#$", "Button1 has been pressed")
-            textView.setText("Button1 has been pressed")
+            textView.text = "Button1 has been pressed"
         }
         button2.setOnClickListener {
             Log.e("@#$", "Button2 has been pressed")
-            textView.setText("Button2 has been pressed")
+            textView.text = "Button2 has been pressed"
         }
         button3.setOnClickListener {
             Log.e("@#$", "Button3 has been pressed")
-            textView.setText("Button3 has been pressed")
+            textView.text = "Button3 has been pressed"
         }
         button4.setOnClickListener {
             Log.e("@#$", "Button4 has been pressed")
-            textView.setText("Button4 has been pressed")
+            textView.text = "Button4 has been pressed"
         }
     }
 }
