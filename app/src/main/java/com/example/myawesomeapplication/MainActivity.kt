e7b175f092d38baba38139972c434cc4d238fe36
Roman Klyucharev <refish@tuta.io>
2020-10-13 16:24:59 +0500

    Text on screen

--- app/src/main/java/com/example/myawesomeapplication/MainActivity.kt
+++ app/src/main/java/com/example/myawesomeapplication/MainActivity.kt
@@ -12,16 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener {
-            Log.e("@#$", "Button1 have been pressed")
+            Log.e("@#$", "Button1 has been pressed")
+            textView.setText("Button1 has been pressed")
         }
         button2.setOnClickListener {
-            Log.e("@#$", "Button2 have been pressed")
+            Log.e("@#$", "Button2 has been pressed")
+            textView.setText("Button2 has been pressed")
         }
         button3.setOnClickListener {
-            Log.e("@#$","Button3 have been pressed")
+            Log.e("@#$", "Button3 has been pressed")
+            textView.setText("Button3 has been pressed")
         }
         button4.setOnClickListener {
-            Log.e("@#$", "Button4 have been pressed")
+            Log.e("@#$", "Button4 has been pressed")
+            textView.setText("Button4 has been pressed")
         }
     }
 }
